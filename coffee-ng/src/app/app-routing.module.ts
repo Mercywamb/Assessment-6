7e2bd4afe0a20831aa257fba2431b97c5f620d86
MAC <mac@MACs-MacBook-Pro.local>
2023-11-17 12:03:02 +0300

    coffee

--- coffee-ng/src/app/app-routing.module.ts
+++ coffee-ng/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component:HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'nav', component: NavComponent },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
